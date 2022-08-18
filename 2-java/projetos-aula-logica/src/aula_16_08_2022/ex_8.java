49e7151ad3851936c9eff99dd5d4953743418d23
guilherme.schroder <guilhermeschroder@outlook.com>
2022-08-18 13:21:52 -0300

    ok

--- 2-java/projetos-aula-logica/src/aula_16_08_2022/ex_8.java
+++ 2-java/projetos-aula-logica/src/aula_16_08_2022/ex_8.java
@@ -5,34 +5,22 @@ import java.util.Scanner;
 public class ex_8 {
     public static void main(String[] args) {
 
-        //Faça um algoritmo que leia um nome e o guarde na memória.
-        //Após a leitura, verifique se a palavra é um palíndromo.
-        //Um palíndromo é aquela palavra que a sua leitura
-        //é a mesma da esquerda para a direita e vice versa. Exemplo: ARARA, ANA, etc.
-        //(usar função strlen() do JAVA).
-        // (A função strlen retorna o número de caracteres inseridos numa variável.
 
-        Scanner teclado = new Scanner(System.in);
-
-        String nomes;
-        int tamanho =0;
-        String nomesinvertidos = " ";
-
-            System.out.println("Informe um nome:");
-            nomes =teclado.next();
-
-
-            tamanho=nomes.length();
-
-            //for(int i=tamanho-1;i>=0;i--){
-           //      nomesinvertidos += .charAt(i);
-            //}
-
-         if(nomes.equals(nomesinvertidos)) {
-             System.out.println(null,nomes + " É uma palavra palindroma");
-         } else {
-             System.out.println(nomes + " Não é uma palavra palindroma");
-         }
-
+        Scanner sc = new Scanner(System.in);
+        String palindromo;
+        boolean pali = true;
+        System.out.println("Insira uma palavra:");
+        palindromo = sc.next();
+        int len = palindromo.length();
+        for(int i = 0; i < len; i++) {
+            if (palindromo.charAt(i) != palindromo.charAt(len - i - 1)) {
+                pali = false;
+            }
+        }
+        if (pali) {
+            System.out.println(palindromo + " é palíndromo!");
+        } else {
+            System.out.println(palindromo + " não é palíndromo!");
+        }
     }
 }
